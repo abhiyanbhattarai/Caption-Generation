9507e72a4c6d42ac1b4f08697910a13376d549f6
Abhiyan Bhattarai <abhiyanbhattarai88@gmail.com>
2023-11-16 08:09:03 -0500

    Adding Comments

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, send_from_directory, flash, redirect, url_for, session, current_app
+from flask import Blueprint, render_template, request, send_from_directory, flash, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
 from flask_login import login_required
@@ -7,7 +7,6 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 from prediction import generate_caption, model1, EncoderDecoder
-# from datetime import datetime, timedelta, timezone
 from instagram_posting import post_to_instagram
 from gpt_integration import generate_gpt_caption  
 
@@ -23,7 +22,6 @@ UPLOAD_FOLDER = 'website/static/uploads/'
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    # check_session_timeout()  # Apply session timeout check before processing the /home route
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
@@ -34,14 +32,13 @@ def home():
             # Generate the image caption
             generated_caption = generate_caption(file_path,model1())
             
-            # Retrieve the prompt_type from the form data
-            # prompt_type = request.form.get('prompt_type')s
-
+            # Get the sentiment from the form
             sentiment = request.form['sentiment']
 
-            better_caption = generate_gpt_caption(generated_caption, sentiment)  # Call your GPT-3 integration function
+            # Generate a better caption using GPT-3
+            better_caption = generate_gpt_caption(generated_caption, sentiment)  
 
-
+            # Display the image caption
             flash('Image Caption Generated and Displayed Below!')
             return render_template('home.html', filename=filename, better_caption=better_caption)
 
