55a6992702ef2fc44eb944c23741a41b542886d0
Abhiyan Bhattarai <abhiyanbhattarai88@gmail.com>
2023-11-18 10:00:53 -0500

    Update

--- website/models.py
+++ website/models.py
@@ -2,14 +2,14 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-
+# This is the database model for the user and the notes
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
+# This is the database model for the user
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
